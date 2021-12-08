37cb540125e3e450d48a7bfe27561fd2b7e38acf
mariaandreescu <maria.andreescu@stud.acs.upb.com>
2021-12-08 14:26:00 +0200

    add firebase built

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -5,24 +5,3 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDmjc8Vm1dz-HutY110K6if58HimrIB1vQ",
-  authDomain: "artistryhub-9bc66.firebaseapp.com",
-  projectId: "artistryhub-9bc66",
-  storageBucket: "artistryhub-9bc66.appspot.com",
-  messagingSenderId: "707571423945",
-  appId: "1:707571423945:web:34893ff65eb106e1844b18",
-  measurementId: "${config.measurementId}"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
